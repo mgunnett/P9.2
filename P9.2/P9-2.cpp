8b228a2afe2b4d6559cbc70c1a23c28632a5a4ae
mgunnett <m-gunnett@onu.edu>
2024-11-07 15:57:12 -0500

    Completed isSortedIncreasing

--- P9.2/P9-2.cpp
+++ P9.2/P9-2.cpp
@@ -1,1 +1,35 @@
- 
+#include <iostream>
+
+using namespace std;
+
+int isSortedIncreasing(int array[], int size);
+
+int main() {
+	int size = 0;
+
+	cout << "How many numbers are you entering? ";
+	cin >> size;
+
+	int* array = new int[size];
+	
+	for (int i = 0; i < size; i++) {
+		cout << "Enter integer #" << i + 1 << ": ";
+		cin >> array[i];
+	}
+
+	if (isSortedIncreasing(array, size) == true) {
+		cout << "\nNumbers are increasing" << endl;
+	}
+	else if (isSortedIncreasing(array, size) == false) {
+		cout << "\nNumbers are not increasing" << endl;
+	}
+}
+
+int isSortedIncreasing(int array[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		if (array[i] > array[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
